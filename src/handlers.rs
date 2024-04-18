c6b5117b4f92e9b66643e55c8b8aa6f8cbc40bc9
Ben Dowling <bendowlingtech@gmail.com>
2024-04-18 18:39:04 +0100

    i love Rust

---
+++ src/handlers.rs
@@ -0,0 +1,16 @@
+
+pub fn create_post(id: u64) {
+
+}
+
+pub fn get_post(id: u64) {
+
+}
+
+pub fn delete_post(id: u64) {
+
+}
+
+pub fn update_post(id: u64) {
+
+}
---
+++ src/models.rs
@@ -0,0 +1,7 @@
+struct Post {
+    title: String,
+    content: String
+}
+
+
+
